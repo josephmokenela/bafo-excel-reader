50161bf12cdb1d28e696c93d7ba331b2cc36f74c
Joseph Mokenela <joseph.mokenela@gmail.com>
2018-12-08 10:47:32 +0200

    Initial commit

---
+++ src/main/java/bafo/CellFormater.java
@@ -0,0 +1,35 @@
+package bafo;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+
+public class CellFormater {
+
+    private static void printCellValue(Cell cell) {
+        switch (cell.getCellTypeEnum()) {
+            case BOOLEAN:
+                System.out.print(cell.getBooleanCellValue());
+                break;
+            case STRING:
+                System.out.print(cell.getRichStringCellValue().getString());
+                break;
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    System.out.print(cell.getDateCellValue());
+                } else {
+                    System.out.print(cell.getNumericCellValue());
+                }
+                break;
+            case FORMULA:
+                System.out.print(cell.getCellFormula());
+                break;
+            case BLANK:
+                System.out.print("");
+                break;
+            default:
+                System.out.print("");
+        }
+
+        System.out.print("\t");
+    }
+}
